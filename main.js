1481636b9968450d6c864cb7844680ac6d67e586
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-05-01 22:25:07 -0400

    Change background to linear gradient

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-const colors = ["red", "green", "blue", "purple", "orange"];
+const colors = [
+  "red",
+  "green",
+  "blue",
+  "purple",
+  "orange",
+  "yellow",
+  "gray",
+  "orange",
+  "pink",
+];
 
 function isElementOutOfViewport(element) {
   const rect = element.getBoundingClientRect();
@@ -14,6 +24,19 @@ function getRandomColor() {
   return colors[Math.floor(Math.random() * colors.length)];
 }
 
+// i.e: `linear-gradient(to right, ${COLOR}, ${COLOR})`;
+function createGradientBackgroundStr() {
+  const colorOne = getRandomColor();
+  let colorTwo = getRandomColor();
+
+  // we'll try once if we have a match
+  if (colorOne === colorTwo) {
+    colorTwo = getRandomColor();
+  }
+
+  return `linear-gradient(to right, ${colorOne}, ${colorTwo})`;
+}
+
 document.addEventListener("click", function (event) {
   const x = event.pageX;
   const y = event.pageY;
@@ -24,7 +47,8 @@ document.addEventListener("click", function (event) {
   // st the coords to the click location
   el.style.top = `${y}px`;
   el.style.left = `${x}px`;
-  el.style.backgroundColor = getRandomColor();
+  // el.style.backgroundColor = getRandomColor();
+  el.style.background = createGradientBackgroundStr();
 
   const body = document.body;
   body.appendChild(el);
